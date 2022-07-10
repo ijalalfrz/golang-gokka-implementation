ffcf9556eafc572cbcc9d01c358d40bed5073690
Rizal Alfarizi <ijal.alfarizi@gmail.com>
2022-07-11 02:24:42 +0700

    feat(model): add comment

--- webmodel/wallet.go
+++ webmodel/wallet.go
@@ -1,10 +1,12 @@
 package webmodel
 
+// DepositWalletPayload is model for deposit wallet http request payload
 type DepositWalletPayload struct {
 	WalletId string  `json:"wallet_id" validate:"required"`
 	Amount   float64 `json:"amount" validate:"required"`
 }
 
+// DetailWalletResponse is response for get detail wallet
 type DetailWalletResponse struct {
 	WalletId       string  `json:"wallet_id"`
 	Balance        float64 `json:"balance"`
